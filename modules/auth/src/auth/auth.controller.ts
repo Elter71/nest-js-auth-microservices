c3b64c5af3784c72edc6c3f8cde67e74d2399300
Piotr Wodziski <piotr@wodzinski.org>
2021-11-15 21:59:56 +0100

    add nginx config

--- modules/auth/src/auth/auth.controller.ts
+++ modules/auth/src/auth/auth.controller.ts
@@ -3,12 +3,12 @@ import { MessagePattern } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 
-@Controller()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
-  @Post('auth')
+  @Post('')
   async login(@Request() req) {
     return this.authService.login(req.user);
   }
